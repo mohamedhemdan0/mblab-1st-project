240640d3f0bcd30f8329a62ea93e2173bc53158c
mohamedhemdan0 <mohamed333hemdan@gmail.com>
2021-09-11 13:24:54 +0200

    new files

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: TECHNO
+ *
+ * Created on September 11, 2021, 1:12 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
